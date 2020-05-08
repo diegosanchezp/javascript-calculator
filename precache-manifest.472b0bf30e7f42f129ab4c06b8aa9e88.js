66fd9f8cb198310a5526b0b97edef13ad8bfd298
Diego Sanchez <diegosandmg@gmail.com>
2020-05-08 18:41:43 -0400

    Updates

---
+++ precache-manifest.472b0bf30e7f42f129ab4c06b8aa9e88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867f104d58b714d062b50fff28dc35c8",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "b7607aca89101e0182bd",
+    "url": "/javascript-calculator/static/css/main.163f3d77.chunk.css"
+  },
+  {
+    "revision": "7033784001aeaea56c8d",
+    "url": "/javascript-calculator/static/js/2.1f67cdb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/javascript-calculator/static/js/2.1f67cdb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7607aca89101e0182bd",
+    "url": "/javascript-calculator/static/js/main.3e40e18a.chunk.js"
+  },
+  {
+    "revision": "9fb1b977845cfa8b3de2",
+    "url": "/javascript-calculator/static/js/runtime-main.3cb956cc.js"
+  }
+]);
